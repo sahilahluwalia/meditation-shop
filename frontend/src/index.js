4997052bd5d34cb6f463400da9113ebf0a2293f8
sahilahluwalia <satrader2000@gmail.com>
2021-06-12 15:17:05 +0530

    Revert "converted all compo into redux""
    This reverts commit 379f481d01cd58ee3df8d2dd4a0fd49d7d9e83af.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store';
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
