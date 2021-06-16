12ec2a9bfc83cb73a0b2a0f6e548906f8006a79c
sahilahluwalia <satrader2000@gmail.com>
2021-06-16 16:58:11 +0530

    added shopping cart

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -16,19 +16,21 @@ const HomeScreen = () => {
 
   return (
     <>
-      <h1>Latest Product</h1>
+      <h1>Latest Products</h1>
       {loading ? (
         <Loader />
       ) : error ? (
         <Message variant='danger'>{error}</Message>
       ) : (
-        <Row>
-          {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
+        <>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
       )}
     </>
   );
