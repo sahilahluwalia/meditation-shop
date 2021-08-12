c1b1acd451975a5d74d3d4dffaa3983949faafa4
sahilahluwalia <satrader2000@gmail.com>
2021-08-12 23:26:34 +0530

    ready for production v2

--- backend/server.js
+++ backend/server.js
@@ -47,8 +47,6 @@ app.post('/mydata', function (req, res) {
   res.send('POST request to homepage');
 });
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
